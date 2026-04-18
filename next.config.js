eb1cdc6d23bc4b5e3f6a2b9ccc3fd702b7276e26
NgJinXuan <jinxuanng111@gmail.com>
2026-04-18 15:53:16 +0800

    s

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // 尝试告诉 Next.js 使用 pages router（但有 app/ 目录时通常无效）
+  experimental: {
+    // 通常没用
+  }
 }
 
 module.exports = nextConfig
